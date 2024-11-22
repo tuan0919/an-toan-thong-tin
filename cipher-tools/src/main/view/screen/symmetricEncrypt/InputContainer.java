bb147a7bc87afa89b04078a857b4b405d768957b
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-22 23:56:38 +0700

    chỉnh sửa propertyName mà event propEventChange sẽ lắng nghe

--- cipher-tools/src/main/view/screen/symmetricEncrypt/InputContainer.java
+++ cipher-tools/src/main/view/screen/symmetricEncrypt/InputContainer.java
@@ -22,7 +22,12 @@ public class InputContainer extends JPanel {
     private final JComboBox<String> KeySizeComboBox;
     private final JLabel LabelPadding;
     private final JComboBox<String> PaddingComboBox;
-    public InputContainer() {
+    private final JButton RollKeyButton;
+    private final JButton AcceptKeyButton;
+    private final JLabel LabelOutputText;
+    private final JTextArea TextAreaOutputTextArea;
+
+    public InputContainer(JComponent observable) {
         this.LabelInputFile = new JLabel("File");
         this.TextFieldInputFile = new JTextField();
         this.FileBrowserButton = new JButton("browse");
@@ -34,19 +39,40 @@ public class InputContainer extends JPanel {
         this.ModeComboBox = new JComboBox<>(new String[]{"ECB", "CBC", "OFB", "CFB"});
         this.LabelKey = new JLabel("Key");
         this.TextFieldKey = new JTextField();
+        this.RollKeyButton = new JButton("Roll New Key");
+        this.AcceptKeyButton = new JButton("Accept Key");
         this.ButtonEncrypt = new JButton("Encrypt");
         this.ButtonDecrypt = new JButton("Decrypt");
         this.LabelKeySize = new JLabel("Key size in Bits");
         this.KeySizeComboBox = new JComboBox<>(new String[]{"128", "192", "256"});
         this.LabelPadding = new JLabel("Padding");
         this.PaddingComboBox = new JComboBox<>(new String[]{"NoPadding", "PKCS5Padding", "PKCS7Padding"});
+        this.LabelOutputText = new JLabel("Output text");
+        this.TextAreaOutputTextArea = new JTextArea();
+
+        observable.addPropertyChangeListener("inputType", event -> {
+            String mode = event.getNewValue().toString();
+            this.removeAll();
+            switch (mode) {
+                case "Encrypt Text" -> {
+                    System.out.println("Encrypt Text");
+                    renderEncryptTextMode();
+                    break;
+                }
+                case "Encrypt File" -> {
+                    System.out.println("Encrypt File");
+                    renderEncryptFileMode();
+                    break;
+                }
+            }
+            this.revalidate();
+            this.repaint();
+        });
         this.initialState();
     }
 
-    public void initialState() {
-        var layout = new GridBagLayout();
+    private void renderEncryptFileMode() {
         GridBagConstraints gbc = new GridBagConstraints();
-        this.setLayout(layout);
         gbc.insets = new Insets(5, 5, 5, 5);
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.anchor = GridBagConstraints.NORTH;
@@ -68,21 +94,6 @@ public class InputContainer extends JPanel {
         }
 
         {
-            gbc.gridy = 1;
-            gbc.gridx = 0;
-            gbc.weightx = 0.3;
-            gbc.anchor = GridBagConstraints.NORTHWEST;
-            this.add(LabelInputText, gbc);
-
-            gbc.gridx = 1;
-            gbc.weightx = 0.7;
-            gbc.gridwidth = 2;
-            this.add(TextAreaInputTextArea, gbc);
-            TextAreaInputTextArea.setRows(8);
-            TextAreaInputTextArea.setBorder(new BevelBorder(BevelBorder.LOWERED));
-        }
-
-        {
             gbc.gridy = 2;
             gbc.gridx = 0;
             gbc.weightx = 0.3;
@@ -90,7 +101,7 @@ public class InputContainer extends JPanel {
 
             gbc.gridx = 1;
             gbc.weightx = 0.7;
-            gbc.gridwidth = 2;
+            gbc.gridwidth = 3;
             this.add(FunctionComboBox, gbc);
         }
 
@@ -102,6 +113,7 @@ public class InputContainer extends JPanel {
 
             gbc.gridx = 1;
             gbc.weightx = 0.7;
+            gbc.gridwidth = 3;
             this.add(ModeComboBox, gbc);
         }
 
@@ -112,8 +124,19 @@ public class InputContainer extends JPanel {
             this.add(LabelKey, gbc);
 
             gbc.gridx = 1;
-            gbc.weightx = 0.7;
+            gbc.weightx = 0.5;
+            gbc.gridwidth = 1;
             this.add(TextFieldKey, gbc);
+
+            gbc.gridx = 2;
+            gbc.weightx = 0.1;
+            gbc.gridwidth = 1;
+            this.add(RollKeyButton, gbc);
+
+            gbc.gridx = 3;
+            gbc.weightx = 0.1;
+            gbc.gridwidth = 1;
+            this.add(AcceptKeyButton, gbc);
         }
 
         {
@@ -124,6 +147,7 @@ public class InputContainer extends JPanel {
 
             gbc.gridx = 1;
             gbc.weightx = 0.7;
+            gbc.gridwidth = 2;
             this.add(KeySizeComboBox, gbc);
         }
 
@@ -152,5 +176,127 @@ public class InputContainer extends JPanel {
         gbc.gridy = 8;
         gbc.weighty = 1.0;
         this.add(new JPanel(), gbc);
+    }
+
+    private void renderEncryptTextMode() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.NORTH;
+
+        {
+            gbc.gridy = 1;
+            gbc.gridx = 0;
+            gbc.weightx = 0.3;
+            gbc.anchor = GridBagConstraints.NORTHWEST;
+            this.add(LabelInputText, gbc);
+
+            gbc.gridx = 1;
+            gbc.weightx = 0.7;
+            gbc.gridwidth = 3;
+            this.add(TextAreaInputTextArea, gbc);
+            TextAreaInputTextArea.setRows(8);
+            TextAreaInputTextArea.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        }
+
+        {
+            gbc.gridy = 2;
+            gbc.gridx = 0;
+            gbc.weightx = 0.3;
+            this.add(LabelFunction, gbc);
+
+            gbc.gridx = 1;
+            gbc.weightx = 0.7;
+            gbc.gridwidth = 3;
+            this.add(FunctionComboBox, gbc);
+        }
+
+        {
+            gbc.gridy = 3;
+            gbc.gridx = 0;
+            gbc.weightx = 0.3;
+            this.add(LabelMode, gbc);
+
+            gbc.gridx = 1;
+            gbc.weightx = 0.7;
+            gbc.gridwidth = 3;
+            this.add(ModeComboBox, gbc);
+        }
+
+        {
+            gbc.gridy = 4;
+            gbc.gridx = 0;
+            gbc.weightx = 0.3;
+            this.add(LabelKey, gbc);
+
+            gbc.gridx = 1;
+            gbc.weightx = 0.5;
+            gbc.gridwidth = 1;
+            this.add(TextFieldKey, gbc);
+
+            gbc.gridx = 2;
+            gbc.weightx = 0.1;
+            gbc.gridwidth = 1;
+            this.add(RollKeyButton, gbc);
+
+            gbc.gridx = 3;
+            gbc.weightx = 0.1;
+            gbc.gridwidth = 1;
+            this.add(AcceptKeyButton, gbc);
+        }
+
+        {
+            gbc.gridy = 5;
+            gbc.gridx = 0;
+            gbc.weightx = 0.3;
+            this.add(LabelKeySize, gbc);
+
+            gbc.gridx = 1;
+            gbc.weightx = 0.7;
+            gbc.gridwidth = 2;
+            this.add(KeySizeComboBox, gbc);
+        }
+
+        {
+            gbc.gridy = 6;
+            gbc.gridx = 0;
+            gbc.weightx = 0.3;
+            this.add(LabelPadding, gbc);
+
+            gbc.gridx = 1;
+            gbc.weightx = 0.7;
+            gbc.gridwidth = 2;
+            this.add(PaddingComboBox, gbc);
+        }
+
+        {
+            gbc.gridy = 7;
+            gbc.gridx = 1;
+            gbc.weightx = 0.7;
+            var ButtonWrapper = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            ButtonWrapper.add(this.ButtonEncrypt);
+            ButtonWrapper.add(this.ButtonDecrypt);
+            this.add(ButtonWrapper, gbc);
+        }
+
+        {
+            gbc.gridy = 8;
+            gbc.gridx = 0;
+            gbc.weightx = 0.3;
+            gbc.anchor = GridBagConstraints.NORTHWEST;
+            this.add(LabelOutputText, gbc);
+
+            gbc.gridx = 1;
+            gbc.weightx = 0.7;
+            gbc.gridwidth = 3;
+            this.add(TextAreaOutputTextArea, gbc);
+            TextAreaOutputTextArea.setRows(8);
+            TextAreaOutputTextArea.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        }
+    }
+
+    public void initialState() {
+        var layout = new GridBagLayout();
+        this.setLayout(layout);
     }
 }
