bc1256fd987a3bb138dfda74e1b74d46d27f4996
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-24 03:22:15 +0700

    Thêm View Navigator để quản lí các screen khác nhau của ứng dụng

---
+++ cipher-tools-rework/src/View/Navigator/TabNavigator.java
@@ -0,0 +1,38 @@
+package View.Navigator;
+
+import Controller.*;
+import View.*;
+
+import javax.swing.*;
+
+public class TabNavigator extends JTabbedPane {
+    private TraditionalScreen_View TraditionalScreenView = new TraditionalScreen_View();
+    {
+        new TraditionalScreen_Controller(TraditionalScreenView);
+    }
+    private SymmetricScreen_View SymmetricScreenView = new SymmetricScreen_View();
+    {
+        new SymmetricScreen_Controller(SymmetricScreenView);
+    }
+    private AsymmetricScreen_View AsymmetricScreenView = new AsymmetricScreen_View();
+    {
+        new AsymmetricScreen_Controller(AsymmetricScreenView);
+    }
+    private HashScreen_View HashScreenView = new HashScreen_View();
+    {
+        new HashScreen_Controller(HashScreenView);
+    }
+    private SignatureScreen_View SignatureScreenView = new SignatureScreen_View();
+    {
+        new SignatureScreen_Controller(SignatureScreenView);
+    }
+
+    public TabNavigator() {
+        addTab("Traditional", TraditionalScreenView);
+        addTab("Symmetric", SymmetricScreenView);
+        addTab("Asymmetric", AsymmetricScreenView);
+        addTab("Hash", HashScreenView);
+        addTab("Signature", SignatureScreenView);
+        setSelectedIndex(1); // Set the initial tab to Traditional Screen by default
+    }
+}
