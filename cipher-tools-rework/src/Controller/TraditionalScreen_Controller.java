13c6c747706d61f0ecd3a966113846c3530ea7ca
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-26 13:12:02 +0700

    update  lại giao diện của chữ kí số và mã hóa truyền thống

--- cipher-tools-rework/src/Controller/TraditionalScreen_Controller.java
+++ cipher-tools-rework/src/Controller/TraditionalScreen_Controller.java
@@ -1,12 +1,9 @@
 package Controller;
 
-import Model.Algorithm.MaHoaCoBan.CaesarCipher;
-import Model.Algorithm.MaHoaCoBan.SubstitutionCipher;
 import View.TraditionalScreen_View;
 
 public class TraditionalScreen_Controller extends AController<TraditionalScreen_View> {
-    private CaesarCipher caesarCipher;
-    private SubstitutionCipher substitutionCipher;
+
 
     public TraditionalScreen_Controller(TraditionalScreen_View view) {
         super(view);
@@ -20,37 +17,14 @@ public class TraditionalScreen_Controller extends AController<TraditionalScreen_
 
     @Override
     protected void initialModels() {
-        this.caesarCipher = new CaesarCipher();
-        this.substitutionCipher = new SubstitutionCipher();
+
     }
 
     private void handleEncrypt() {
-        String modeTemp = (String) view.getModeComboBox().getSelectedItem();
-        String inputText = view.getInputTextArea().getText();
-        if (inputText == null || inputText.isEmpty()) {
-            view.showWarningEmptyMessage();
-            return;
-        }
-        String outputText = switch (modeTemp) {
-            case "Dịch chuyển" -> caesarCipher.encrypt(inputText);
-            case "Thay thế" -> substitutionCipher.encrypt(inputText);
-            default -> "";
-        };
-        view.showOutputTextArea(outputText);
+
     }
 
     private void handleDecrypt() {
-        String modeTemp = (String) view.getModeComboBox().getSelectedItem();
-        String inputText = view.getInputTextArea().getText();
-        if (inputText == null || inputText.isEmpty()) {
-            view.showWarningEmptyMessage();
-            return;
-        }
-        String outputText = switch (modeTemp) {
-            case "Dịch chuyển" -> caesarCipher.decrypt(inputText);
-            case "Thay thế" -> substitutionCipher.decrypt(inputText);
-            default -> "";
-        };
-        view.showOutputTextArea(outputText);
+
     }
 }
