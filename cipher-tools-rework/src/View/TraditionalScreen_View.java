9a7948f8b87a5d8fedcd1109b795b5064cd927f7
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-27 19:38:21 +0700

    update giao diện

--- cipher-tools-rework/src/View/TraditionalScreen_View.java
+++ cipher-tools-rework/src/View/TraditionalScreen_View.java
@@ -1,24 +1,46 @@
 package View;
 
 import Util.MyUtil;
+import View.Component.MatrixTable_View;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.TableColumnModelEvent;
+import javax.swing.event.TableColumnModelListener;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.function.Consumer;
 
 public class TraditionalScreen_View extends AScreenView {
-    private JComboBox<String> ModeComboBox;
+    private JComboBox<String> AlgorithmSelector_ComboBox;
     private JTextArea InputTextArea;
     private JTextArea OutputTextArea;
     private JButton EncryptButton;
     private JButton DecryptButton;
     private JScrollPane InputTextWrap_ScrollPane;
     private JScrollPane OutputTextWrap_ScrollPane;
-    private JPanel SelectModeWrap_Panel;
     private JPanel OverallContentWrap_Panel;
     private JPanel CipherContentWrap_Panel;
+    private MatrixTable_View MatrixInput_Table;
+    private JTextField[] MatrixSize_TextField;
+    private JButton CreateMatrix_Button;
+    private JButton SaveMatrix_Button;
+    private JButton LoadMatrix_Button;
+    private JComboBox<String> AlphabetChoose_ComboBox;
+    private JTextField[] InputKeyAB_TextField;
+    private JButton LoadKeyAB_Button;
+    private JButton SaveKeyAB_Button;
+    private JTextField PlainTextAlphabet_TextField;
+    private JTextField CipherTextAlphabet_TextField;
 
     public TraditionalScreen_View() {
         super();
@@ -26,89 +48,197 @@ public class TraditionalScreen_View extends AScreenView {
 
     @Override
     public void initialComponent() {
-        ModeComboBox = new JComboBox<>();
+        AlgorithmSelector_ComboBox = new JComboBox<>(); {
+            AlgorithmSelector_ComboBox.addItem("Caesar");
+            AlgorithmSelector_ComboBox.addItem("Affine");
+            AlgorithmSelector_ComboBox.addItem("Vigenere");
+            AlgorithmSelector_ComboBox.addItem("Substitution");
+            AlgorithmSelector_ComboBox.addItem("Hill");
+        }
         EncryptButton = new JButton("Mã hóa");
         DecryptButton = new JButton("Giải mã");
         InputTextArea = new JTextArea(20, 35);
         OutputTextArea = new JTextArea(20, 35);
         InputTextWrap_ScrollPane = new JScrollPane(InputTextArea);
         OutputTextWrap_ScrollPane = new JScrollPane(OutputTextArea);
-        SelectModeWrap_Panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        OverallContentWrap_Panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        OverallContentWrap_Panel = new JPanel(new GridLayout());
         CipherContentWrap_Panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        MatrixInput_Table = new MatrixTable_View();
+        CreateMatrix_Button = new JButton("Tạo ma trận");
+        SaveMatrix_Button = new JButton("Lưu");
+        LoadMatrix_Button = new JButton("Load");
+        MatrixSize_TextField = new JTextField[] {
+                new JTextField(),
+                new JTextField(),
+        };
+        AlphabetChoose_ComboBox = new JComboBox<>(); {
+            AlphabetChoose_ComboBox.addItem("EN Alphabet - Bảng chữ cái la tinh.");
+            AlphabetChoose_ComboBox.addItem("VI Alphabet - Bảng chữ cái tiếng việt.");
+            AlphabetChoose_ComboBox.addItem("Custom Alphabet - Bảng chữ cái tự định nghĩa.");
+        }
+        InputKeyAB_TextField = new JTextField[] {
+                new JTextField(),
+                new JTextField(),
+        };
+        LoadKeyAB_Button = new JButton("Load key");
+        SaveKeyAB_Button = new JButton("Lưu key");
+        PlainTextAlphabet_TextField = new JTextField();
+        CipherTextAlphabet_TextField = new JTextField();
     }
 
     @Override
     public void initialLayout() {
-        ModeComboBox.addItem("Dịch chuyển");
-        ModeComboBox.addItem("Thay thế");
-
         InputTextWrap_ScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         InputTextWrap_ScrollPane.setBorder(BorderFactory.createTitledBorder("Nguồn"));
         OutputTextWrap_ScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         OutputTextWrap_ScrollPane.setBorder(BorderFactory.createTitledBorder("Kết quả"));
 
-        SelectModeWrap_Panel.add(new JLabel("Chọn chế độ"));
-        SelectModeWrap_Panel.add(ModeComboBox);
+        JPanel AlgorithmSettings = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.gridy = 0; {
+            gbc.weightx = 0.3;
+            gbc.gridx = 0;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.anchor = GridBagConstraints.WEST;
+            AlgorithmSettings.add(new JLabel("Chọn thuật toán:"), gbc);
 
+            gbc.weightx = 0.5;
+            gbc.gridx = 1;
+            gbc.anchor = GridBagConstraints.WEST;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            AlgorithmSettings.add(AlgorithmSelector_ComboBox, gbc);
+        }
+
+        gbc.gridy = 1; {
+            Integer[][] data = new Integer[15][15]; // Ma trận 5x5
+            for (int i = 0; i < data.length; i++) {
+                for (int j = 0; j < data.length; j++) {
+                    data[i][j] = i * data.length + j + 1; // Điền số từ 1 đến 25
+                }
+            }
+            JPanel wrapper = new JPanel(new GridBagLayout()); {
+                var gbc2 = new GridBagConstraints();
+                gbc2.insets = new Insets(0, 5, 0, 5);
+                gbc2.gridy = 0;
+                gbc2.gridx = 0;
+                gbc2.gridheight = GridBagConstraints.REMAINDER;
+                gbc2.gridwidth = 1;
+                gbc2.weightx = 1;
+                gbc2.fill = GridBagConstraints.BOTH;
+                gbc2.anchor = GridBagConstraints.WEST;
+                MatrixInput_Table.setMinCellSize(50);
+                MatrixInput_Table.setViewPortSize(200);
+                MatrixInput_Table.loadModel(data);
+                var tableWrap = new JPanel(new GridLayout());
+                tableWrap.add(MatrixInput_Table);
+                JScrollPane scrollPane = new JScrollPane(tableWrap);
+                scrollPane.setPreferredSize(new Dimension(200, 200));
+                wrapper.add(scrollPane, gbc2);
+
+                gbc2.gridy = 1;
+                gbc2.gridx = 1;
+                gbc2.anchor = GridBagConstraints.NORTHWEST;
+                gbc2.gridheight = 1;
+                gbc2.gridwidth = 1;
+                wrapper.add(new JLabel("Ma trận:"), gbc2);
+
+                gbc2.gridy = 2;
+                gbc2.gridx = 1;
+                gbc2.anchor = GridBagConstraints.NORTHWEST;
+                gbc2.gridheight = 1;
+                gbc2.gridwidth = 1;
+
+                JPanel PanelRow1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+                PanelRow1.add(new JLabel("("));
+                PanelRow1.add(MatrixSize_TextField[0]);
+                MatrixSize_TextField[0].setColumns(1);
+                PanelRow1.add(new JLabel("x"));
+                PanelRow1.add(MatrixSize_TextField[1]);
+                MatrixSize_TextField[1].setColumns(1);
+                PanelRow1.add(new JLabel(")"));
+                wrapper.add(PanelRow1, gbc2);
+
+                gbc2.insets.top = 5;
+                gbc2.gridy = 3;
+                gbc2.gridx = 1;
+                JPanel matrixActionWrapper = new JPanel(new FlowLayout()); {
+                    matrixActionWrapper.add(CreateMatrix_Button);
+                    matrixActionWrapper.add(SaveMatrix_Button);
+                    matrixActionWrapper.add(LoadMatrix_Button);
+                }
+                wrapper.add(matrixActionWrapper, gbc2);
+
+                gbc2.gridy = 4;
+                gbc2.gridx = 1;
+                gbc2.fill = GridBagConstraints.HORIZONTAL;
+                JTextArea txtArea = new JTextArea(); {
+                    txtArea.setEditable(false);
+                    txtArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
+                    txtArea.setText(MyUtil.formatMatrix(data));
+                    txtArea.setColumns(30);
+                    txtArea.setRows(5);
+                }
+                JScrollPane outputMatrix_ScrollPane = new JScrollPane(txtArea); {
+                    outputMatrix_ScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+                    outputMatrix_ScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+                }
+                outputMatrix_ScrollPane.setBorder(new TitledBorder("Ma trận hiện tại"));
+                wrapper.add(outputMatrix_ScrollPane, gbc2);
+            }
+            gbc.gridx = 0;
+            gbc.anchor = GridBagConstraints.WEST;
+            gbc.weightx = 1;
+            gbc.gridwidth = GridBagConstraints.REMAINDER;
+            AlgorithmSettings.add(wrapper, gbc);
+        }
+        gbc.gridy = 2; {
+            gbc.gridx = 0;
+            gbc.anchor = GridBagConstraints.WEST;
+            gbc.fill = GridBagConstraints.NONE;
+            gbc.weightx = 0;
+            AlgorithmSettings.add(new JLabel("Chọn bảng alphabet:"), gbc);
+
+            gbc.weightx = 1;
+            gbc.gridx = 1;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            AlgorithmSettings.add(AlphabetChoose_ComboBox, gbc);
+        }
+        gbc.gridy = 3; {
+            gbc.gridx = 0;
+            gbc.anchor = GridBagConstraints.WEST;
+            gbc.weightx = 0;
+            gbc.fill = GridBagConstraints.REMAINDER;
+            var wrapper = new JPanel(new FlowLayout());
+            wrapper.add(new JLabel("Hệ số k ("));
+            wrapper.add(InputKeyAB_TextField[0]); {
+                InputKeyAB_TextField[0].setColumns(1);
+                InputKeyAB_TextField[0].setHorizontalAlignment(SwingConstants.CENTER);
+            }
+            wrapper.add(new JLabel(","));
+            wrapper.add(InputKeyAB_TextField[1]); {
+                InputKeyAB_TextField[1].setColumns(1);
+                InputKeyAB_TextField[1].setHorizontalAlignment(SwingConstants.CENTER);
+            }
+            wrapper.add(new JLabel(")"));
+            wrapper.add(LoadKeyAB_Button);
+            wrapper.add(SaveKeyAB_Button);
+            AlgorithmSettings.add(wrapper, gbc);
+        }
         OverallContentWrap_Panel.add(InputTextWrap_ScrollPane);
         OverallContentWrap_Panel.add(OutputTextWrap_ScrollPane);
 
         CipherContentWrap_Panel.add(EncryptButton);
         CipherContentWrap_Panel.add(DecryptButton);
 
+        var AlgorithmSettingsWrapper = new JPanel(); {
+            AlgorithmSettingsWrapper.add(AlgorithmSettings);
+        }
+
         this.setLayout(new BorderLayout());
-        this.add(SelectModeWrap_Panel, BorderLayout.NORTH);
+        this.add(AlgorithmSettingsWrapper, BorderLayout.NORTH);
         this.add(OverallContentWrap_Panel, BorderLayout.CENTER);
         this.add(CipherContentWrap_Panel, BorderLayout.SOUTH);
-    }
-
-    public JComboBox<String> getModeComboBox() {
-        return ModeComboBox;
-    }
-
-    public JTextArea getInputTextArea() {
-        return InputTextArea;
-    }
-
-    public JTextArea getOutputTextArea() {
-        return OutputTextArea;
-    }
-
-    public JButton getEncryptButton() {
-        return EncryptButton;
-    }
-
-    public JButton getDecryptButton() {
-        return DecryptButton;
-    }
-
-    public JScrollPane getInputTextWrap_ScrollPane() {
-        return InputTextWrap_ScrollPane;
-    }
-
-    public JScrollPane getOutputTextWrap_ScrollPane() {
-        return OutputTextWrap_ScrollPane;
-    }
-
-    public JPanel getSelectModeWrap_Panel() {
-        return SelectModeWrap_Panel;
-    }
-
-    public JPanel getOverallContentWrap_Panel() {
-        return OverallContentWrap_Panel;
-    }
-
-    public JPanel getCipherContentWrap_Panel() {
-        return CipherContentWrap_Panel;
-    }
-
-    public void showOutputTextArea(String content) {
-        this.OutputTextArea.setText(content);
-    }
-
-    public void showWarningEmptyMessage() {
-        MyUtil.showWarnMessage(this, "Vui lòng nhập nội dung văn bản", "Thông báo");
     }
 
     public void onEncryptButton_Click(Consumer<ActionEvent> callback) {
